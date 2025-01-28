1f2a49dc48f4816d181a2ac8dbed7178dc722ca7
Vo Thach Chi Truong <vtchitruong@gmail.com>
2025-01-28 11:53:27 +0700

    Init

---
+++ calendar/my_calendar.py
@@ -0,0 +1,40 @@
+import calendar as cld
+
+# Hàm in lịch theo năm
+def print_year_calendar(year=2025, width=2, lines=1, spacing=6, c=3):
+    # Tạo đối tượng lịch
+    cal = cld.TextCalendar()
+    
+    # In lịch theo năm
+    print(cal.formatyear(year, width, lines, spacing, c))
+
+
+# Hàm in lịch theo tháng
+def print_month_calendar(year=2025, month=1, width=2, lines=1):
+    # Tạo đối tượng lịch
+    cal = cld.TextCalendar()
+    
+    # In lịch theo tháng
+    print(cal.formatmonth(year, month, width, lines))
+
+
+if __name__ == '__main__':
+    while True:
+        print("\nChọn loại lịch:")
+        print("1. Lịch năm")
+        print("2. Lịch tháng")
+        print("0. Thoát")
+
+        choice = input("Nhập lựa chọn: ")
+
+        if choice == '1':
+            year = int(input('Nhập năm: '))
+            print_year_calendar(year)
+        elif choice == '2':
+            year = int(input('Nhập năm: '))
+            month = int(input("Nhập tháng: "))
+            print_month_calendar(year, month)
+        elif choice == '0':
+            break
+        else:
+            print("Lựa chọn không hợp lệ.")
